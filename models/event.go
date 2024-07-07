4e584332efaaee253650c511ea2450822b654bfc
Vikas Malviya <vikas.comau@gmail.com>
2024-07-07 21:28:01 +1000

    logic to create insert table records prepared stmt

--- models/event.go
+++ models/event.go
@@ -1,5 +1,7 @@
 package models
 
+import "example.com/restapi/db"
+
 type Event struct {
 	ID          int    `json:"id"`
 	Name        string `json:"name" binding:"required"`
@@ -11,9 +13,29 @@ type Event struct {
 
 var events = []Event{}
 
-func Save(e Event) {
+func Save(e Event) error {
 	//laer to in deb
-	events = append(events, e)
+	query := `INSERT INTO events (name, description, location, dateTime, userID) 
+	VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		panic(err)
+	}
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+	lastID, err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+	e.ID = int(lastID)
+
+	return err
+
+	//events = append(events, e)
 }
 
 func GetAllEvents() []Event {
